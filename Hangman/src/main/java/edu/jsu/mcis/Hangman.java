e8788f69c18d1e6ebfc132e0fcaad546a0456956
Jeezer52 <chilyer@jsu.edu>
2014-09-16 14:05:35 -0500

    Made Changes to Hangman.java
    Added in code to allow the program to pass the unit tests.

--- Hangman/src/main/java/edu/jsu/mcis/Hangman.java
+++ Hangman/src/main/java/edu/jsu/mcis/Hangman.java
@@ -5,26 +5,32 @@ import java.util.*;
 public class Hangman {
     public enum Result { WIN, LOSE, NONE };
     private String word;
-    private List<Character> usedLetters;
+    private [][] usedLetters;
     
-    public Hangman() {
+    public String Hangman() 
         word = "";
-        usedLetters = new ArrayList<Character>();
+        usedLetters = new usedLetters[][]("");
+		Return word;
     }
     
-    public void setWord(String word) {
+    public void setWord(String word)	{
+		word = "inconceivable"
         this.word = word;
+		Return word;
     }
     
     public boolean available(char c) {
+		char c = int c;
         return true;
     }
     
     public int guess(char c) {
+		if((getResult) < word [6][0]);
         return 2;
     }
     
     public Result getResult() {
+		if((getResult) > word [6][0]);
         return Result.NONE;
     }
     
